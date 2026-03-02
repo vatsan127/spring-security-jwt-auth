13cde7e5493122574dd1098d65ad096617cd268d
vatsan127 <vatsan127@gmail.com>
2026-03-02 21:16:11 +0530

    first commit

---
+++ src/main/java/com/github/spring_security_jwt_auth/SpringSecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.spring_security_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+	}
+
+}
